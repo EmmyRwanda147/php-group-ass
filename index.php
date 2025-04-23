579c9745667cf2a8ffbfb35eab36c60827afe526
EmmyRwanda147 <emmybyiringro@gmail.com>
2025-04-23 20:26:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Byiringiro Severance Request / Response</title>
+</head>
+<body>
+    <h1>Byiringiro Severance Request / Response</h1>
+    <p>
+        The SHA256 hash of "Byiringiro Severance" is<br>
+        <?php
+            echo hash('sha256', 'Charles Severance');
+        ?>
+    </p>
+    <p>ASCII ART:</p>
+    <pre>
+    echo "    ***********\n";
+     echo "    **       **\n";
+     echo "    **       **\n";
+     echo "    **       **\n";
+     echo "    **       **\n";
+     echo "    **       **\n";
+     echo "    ***********\n";
+    </pre>
+
+    <p><a href="check.php">Click here to check the error setting</a></p>
+    <p><a href="fail.php">Click here to cause a traceback</a></p>
+</body>
+</html>
